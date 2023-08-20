aa5a630e2a6b379f7155378ff3e019128d885dec
Joaquín Corugedo <joaquin_coru@hotmail.com>
2023-08-21 00:12:34 +0200

    Initial Commit

---
+++ Beerkipedia/BeerkipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeerkipediaApp.swift
+//  Beerkipedia
+//
+//  Created by Joaquín Corugedo Rodríguez on 21/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BeerkipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beerkipedia/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Beerkipedia
+//
+//  Created by Joaquín Corugedo Rodríguez on 21/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
